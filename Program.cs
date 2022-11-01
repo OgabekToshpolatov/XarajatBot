0aa7da7bacb5cf308ceff2e73ad818321da67ca0
Ogabek <ogabektoshpolatov2002@gmail.com>
2022-11-01 23:51:49 +0500

    Added BackgroundService

--- Program.cs
+++ Program.cs
@@ -1,9 +1,13 @@
 using Telegram.Bot;
+using Xarajat.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 var token = builder.Configuration.GetValue("BotToken", string.Empty);
+
 builder.Services.AddSingleton(new TelegramBotClient(token));
+builder.Services.AddHostedService<BotBackgroundService>();
+
 var app = builder.Build();
 
 app.Run();
--- Services/BotBackgroundService.cs
+++ Services/BotBackgroundService.cs
@@ -1,6 +1,35 @@
-namespace Xarajat;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
 
-public class BotBackgroundService
+namespace Xarajat.Services;
+
+public class BotBackgroundService : BackgroundService
 {
-    
+    private readonly ILogger<BotBackgroundService> _logger;
+    private readonly TelegramBotClient _client;
+    private readonly IUpdateHandler _handler;
+
+    public BotBackgroundService(ILogger<BotBackgroundService> logger,
+    TelegramBotClient client, IUpdateHandler handler)
+    {
+        _logger = logger;
+        _client = client;
+         _handler = handler;
+        
+    }
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+         var bot = await _client.GetMeAsync(stoppingToken);
+
+        _logger.LogInformation("Bot started succesfully:{bot.Username}",bot.Username);
+
+        _client.StartReceiving(
+            _handler.HandleUpdateAsync,
+            _handler.HandlePollingErrorAsync,
+            new ReceiverOptions()
+        {
+            ThrowPendingUpdates = true
+        },
+        stoppingToken);
+    }
 }
