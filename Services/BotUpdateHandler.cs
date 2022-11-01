72fe5b7edc73ac069b1dcde90baee20301da202a
Ogabek <ogabektoshpolatov2002@gmail.com>
2022-11-01 23:56:15 +0500

    Added 2 method

--- Services/BotUpdateHandler.cs
+++ Services/BotUpdateHandler.cs
@@ -1,11 +1,18 @@
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace Xarajat.Services;
 
 public class BotUpdateHandler : IUpdateHandler
 {
+    private readonly ILogger<BotUpdateHandler> _logger;
+
+    public BotUpdateHandler(ILogger<BotUpdateHandler> logger)
+    {
+        _logger = logger;
+    }
     public Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
     {
         throw new NotImplementedException();
@@ -13,6 +20,9 @@ public class BotUpdateHandler : IUpdateHandler
 
     public Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
     {
-        throw new NotImplementedException();
+        var handler = update.Type switch
+        {
+            UpdateType.Message => HandleMessageAsync(botClient, update.Message, cancellationToken),
+        };
     }
 }
